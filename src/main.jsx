68f3f0a40fd2eff92f1d69d876e2d4747b066abc
spencer-rafada <spencer.byui@gmail.com>
2023-06-14 11:37:40 -0700

    added theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import 'normalize.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { ErrorPage } from './pages/index.jsx'
 
 const router = createBrowserRouter([
@@ -13,8 +14,18 @@ const router = createBrowserRouter([
   },
 ])
 
+const appTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff00ff',
+    },
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={appTheme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 )
