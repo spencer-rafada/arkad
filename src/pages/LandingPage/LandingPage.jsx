c51f3610d2881add4ea14965c18cddd6b81f0f90
spencer-rafada <spencer.byui@gmail.com>
2023-06-23 01:02:56 -0600

    add boiler for background color

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,3 +1,20 @@
+import { Container, Box } from '@mui/material'
+import { css } from '@emotion/react'
+import { useTheme } from '@mui/material/styles'
+
 export default function LandingPage() {
-  return <div>LandingPage</div>
+  const theme = useTheme()
+
+  // Styles
+  const containerStyle = css`
+    background-color: ${theme.palette.primary.main};
+    color: ${theme.palette.primary.primaryBtn};
+    width: 100%;
+    height: 1080px;
+  `
+  return (
+    <Container maxWidth='xl' sx={containerStyle}>
+      <Box sx={{ backgroundColor: 'red' }}>y</Box>
+    </Container>
+  )
 }
