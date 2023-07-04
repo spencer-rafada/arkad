361330f82c2235c76c2fcb2ffde38323ccdbd5d3
spencer-rafada <spencer.byui@gmail.com>
2023-07-04 12:54:51 -0600

    added question and button style

--- src/pages/my-goals/dialogs/GoalsDialog.jsx
+++ src/pages/my-goals/dialogs/GoalsDialog.jsx
@@ -1,6 +1,22 @@
 import { useContext } from 'react'
 import { GoalsCheckoutContext } from '../../../providers/GoalsCheckoutProvider'
 
+const questions = [
+  { question: 'What is your monthly revenue?', quote: '' },
+  {
+    question: 'Do you separate money for savings whenever you get paid?',
+    quote: '',
+  },
+  { question: 'Do you have a savings account?', quote: '' },
+  {
+    question:
+      'Are you saving for something specific? (vacation, car, house, etc...)',
+    quote: '',
+  },
+  { question: 'When do you want to achieve this goal?', quote: '' },
+  { question: 'Customize your goal.', quote: '' },
+]
+
 export default function GoalsDialog() {
   const { progress, dialog } = useContext(GoalsCheckoutContext)
   return (
@@ -8,6 +24,7 @@ export default function GoalsDialog() {
       <p>
         {progress} of 5: {dialog}
       </p>
+      <h2>{questions[progress].question}</h2>
     </div>
   )
 }
--- src/pages/my-goals/dialogs/GoalsDialogPicker.jsx
+++ src/pages/my-goals/dialogs/GoalsDialogPicker.jsx
@@ -1,10 +1,14 @@
 import { GoalsCheckoutContext } from '../../../providers/GoalsCheckoutProvider'
 import GoalsDialog from './GoalsDialog'
 import { useContext, useEffect } from 'react'
+import { Stack, Button } from '@mui/material'
+import { css } from '@emotion/react'
+import { useTheme } from '@mui/material/styles'
 
 export default function GoalsDialogPicker() {
   const { dialog, setDialog, setProgress, progress } =
     useContext(GoalsCheckoutContext)
+  const theme = useTheme()
 
   useEffect(() => {
     if (dialog === 'monthlyRevenue') setDialog('monthlyRevenue')
@@ -22,26 +26,46 @@ export default function GoalsDialogPicker() {
     if (progress === 4) setDialog('modifyGoal')
   }, [progress, setDialog])
 
+  // Styles
+  const primaryButtonStyle = css`
+    background-color: ${theme.palette.primary.primaryBtn};
+    color: ${theme.palette.primary.text};
+    &:hover {
+      background-color: ${theme.palette.primary.primaryBtn};
+    }
+  `
+  const secondaryButtonStyle = css`
+    background-color: ${theme.palette.primary.main};
+    color: ${theme.palette.primary.text};
+    &:hover {
+      background-color: ${theme.palette.primary.main};
+    }
+  `
+
   return (
     <div>
       <GoalsDialog />
-      <button
-        disabled={progress === 0}
-        onClick={() => {
-          setProgress(progress !== 0 ? progress - 1 : 0)
-        }}
-      >
-        Back
-      </button>
-      <button
-        disabled={progress === 5}
-        onClick={() => {
-          // need to make the 5 dynamic
-          setProgress(progress !== 4 ? progress + 1 : 5)
-        }}
-      >
-        Next
-      </button>
+      <Stack direction='row' spacing={2}>
+        <Button
+          sx={secondaryButtonStyle}
+          disabled={progress === 0}
+          onClick={() => {
+            setProgress(progress !== 0 ? progress - 1 : 0)
+          }}
+        >
+          Back
+        </Button>
+        <Button
+          sx={primaryButtonStyle}
+          disabled={progress === 5}
+          onClick={() => {
+            // need to make the 5 dynamic
+            setProgress(progress !== 4 ? progress + 1 : 5)
+          }}
+        >
+          Next
+        </Button>
+      </Stack>
     </div>
   )
 }
